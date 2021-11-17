96ff0f59b4000ee268fc2438448d5140818c6f8b
Roylyre <93934853+Roylyre@users.noreply.github.com>
2021-11-17 21:57:00 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+echo "=================="
+echo "Bash script for routersploit installation on termux"
+echo "Keep your phone alive and try not to use it while installing"
+echo "=================="
+sleep 3
+apt update -y && apt upgrade -y
+apt install autoconf automake bison bzip2 clang cmake coreutils diffutils flex gawk git grep gzip libtool make patch perl sed silversearcher-ag tar wget pkg-config python clang libcrypt libffi git openssl libsodium -y
+python -m pip install wheel
+SODIUM_INSTALL=system pip install pynacl
+git clone https://github.com/threat9/routersploit
+python -m pip install requirements.txt
+python -m pip install requirements-dev.txt
+echo "Done!"
